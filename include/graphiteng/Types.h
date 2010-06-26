a362d4ed717d51078dc93e81b0df0d2a5ca72901
Peter Ruffhead peter <Peter Ruffhead peter@buttercups.me.uk>
2010-06-26 23:37:37 +0100

    Speed up Position and Rect by use of &

--- include/graphiteng/Types.h
+++ include/graphiteng/Types.h
@@ -33,7 +33,7 @@ public:
     Position() : x(0), y(0) { }
     Position(const Position &other) {x = other.x, y = other.y;}
     Position(float inx, float iny) { x = inx; y = iny; }
-    Position operator + (Position a) { return Position(x + a.x, y + a.y); }
+    Position operator + (const Position& a) { return Position(x + a.x, y + a.y); }
     Position &operator += (const Position &a) { x += a.x; y += a.y; return *this; }
 
     float x;
@@ -45,9 +45,9 @@ class GRNG_EXPORT Rect
 public :
 
     Rect() {}
-    Rect(Position origin, Position extent) { bl = origin; tr = extent; }
+    Rect(const Position& origin, const Position& extent) { bl = origin; tr = extent; }
     Rect(const Rect &other) {bl = other.bl; tr = other.tr;}
-    Rect widen(Rect other) { return Rect(Position(bl.x > other.bl.x ? other.bl.x : bl.x, bl.y > other.bl.y ? other.bl.y : bl.y), Position(tr.x > other.tr.x ? tr.x : other.tr.x, tr.y > other.tr.y ? tr.y : other.tr.y)); }
+    Rect widen(const Rect& other) { return Rect(Position(bl.x > other.bl.x ? other.bl.x : bl.x, bl.y > other.bl.y ? other.bl.y : bl.y), Position(tr.x > other.tr.x ? tr.x : other.tr.x, tr.y > other.tr.y ? tr.y : other.tr.y)); }
     Rect operator + (const Position &a) const { return Rect(Position(bl.x + a.x, bl.y + a.y), Position(tr.x + a.x, tr.y + a.y)); }
 
     Position bl;
