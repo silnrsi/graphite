4558581af776ff412c64161fba34db9ef0a1b47c
Martin Hosken <martin_hosken@sil.org>
2010-09-02 14:56:20 +0700

    Add LGPL2+ license and remove autotools

--- src/FeatureMap.h
+++ src/FeatureMap.h
@@ -1,3 +1,24 @@
+/*  GRAPHITENG LICENSING
+
+    Copyright 2010, SIL International
+    All rights reserved.
+
+    This library is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 2.1 of License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should also have received a copy of the GNU Lesser General Public
+    License along with this library in the file named "LICENSE".
+    If not, write to the Free Software Foundation, Inc., 59 Temple Place, 
+    Suite 330, Boston, MA 02111-1307, USA or visit their web page on the 
+    internet at http://www.fsf.org/licenses/lgpl.html.
+*/
 #pragma once
 
 #include "graphiteng/Types.h"
