5d6e3a681e2f86e139fcff674c63835e38602692
Keith Stribley <devel@thanlwinsoft.org>
2010-12-14 18:56:02 +0630

    Fix C examples - types need to be defined at top of functions.

--- tests/examples/features.c
+++ tests/examples/features.c
@@ -8,21 +8,27 @@ int main(int argc, char **argv)
     gr_uint16 langId = 0x0409;
     gr_uint32 lang = 0;
     char idtag[5] = {0, 0, 0, 0, 0};                                /*<1>*/
+    struct GrFeatureVal *features = NULL;
+    struct GrFace *face = gr_make_file_face(argv[1], 0);
+    int num = gr_face_n_fref(face);
+    
 
-    GrFace *face = gr_make_file_face(argv[1], 0);
     if (!face) return 1;
     if (argc > 2) lang = gr_str_to_tag(argv[2]);
-    GrFeatureVal *features = gr_face_featureval_for_lang(face, lang);  /*<2>*/
+    features = gr_face_featureval_for_lang(face, lang);  /*<2>*/
     if (!features) return 2;
-    int num = gr_face_n_fref(face);
     for (i = 0; i < num; ++i)
     {
         const GrFeatureRef *fref = gr_face_fref(face, i);              /*<3>*/
         gr_uint32 length = 0;
         char *label = gr_fref_label(fref, &langId, gr_utf8, &length);  /*<4>*/
+        gr_uint32 id = gr_fref_id(fref);                               /*<5>*/
+        gr_uint16 val = gr_fref_feature_value(fref, features);
+        int numval = gr_fref_n_values(fref);
+        int j;
+
         printf("%s ", label);
         gr_label_destroy(label);
-        gr_uint32 id = gr_fref_id(fref);                               /*<5>*/
         if (id <= 0x00FFFFFF)
             printf("(%d)\n", id);
         else
@@ -30,9 +36,6 @@ int main(int argc, char **argv)
             gr_tag_to_str(id, idtag);
             printf("(%s)\n", idtag);
         }
-        gr_uint16 val = gr_fref_feature_value(fref, features);
-        int numval = gr_fref_n_values(fref);
-        int j;
 
         for (j = 0; j < numval; ++j)
         {
--- tests/examples/simple.c
+++ tests/examples/simple.c
@@ -10,18 +10,20 @@ int main(int argc, char **argv)
     int dpi = 96;               /* work with this many dots per inch */
 
     char *pError;               /* location of faulty utf-8 */
-
-    GrFace *face = gr_make_file_face(argv[1], 0);                             /*<1>*/
+    struct GrFont *font = NULL;
+    size_t numCodePoints = 0;
+    struct GrSegment * seg = NULL;
+    const GrSlot *s;
+    struct GrFace *face = gr_make_file_face(argv[1], 0);                             /*<1>*/
     if (!face) return 1;
-    GrFont *font = gr_make_font(pointsize * dpi / 72, face);                   /*<2>*/
+    font = gr_make_font(pointsize * dpi / 72.0f, face);                   /*<2>*/
     if (!font) return 2;
-    size_t numCodePoints = gr_count_unicode_characters(gr_utf8, argv[2], NULL,
+    numCodePoints = gr_count_unicode_characters(gr_utf8, argv[2], NULL,
                 (const void **)(&pError));                                  /*<3>*/
     if (pError) return 3;
-    GrSegment *seg = gr_make_seg(font, face, 0, 0, gr_utf8, argv[2], numCodePoints, rtl); /*<4>*/
+    seg = gr_make_seg(font, face, 0, 0, gr_utf8, argv[2], numCodePoints, rtl); /*<4>*/
     if (!seg) return 3;
 
-    const GrSlot *s;
     for (s = gr_seg_first_slot(seg); s; s = gr_slot_next_in_segment(s))           /*<5>*/
         printf("%d(%f,%f) ", gr_slot_gid(s), gr_slot_origin_X(s), gr_slot_origin_Y(s));
     gr_seg_destroy(seg);
