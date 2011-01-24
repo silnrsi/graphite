f47c89a9e30955d0dd575f9eb1b4c70493328406
Tim Eves <tim_eves@sil.org>
2011-01-24 14:18:23 +0700

    Improve file corrupter
    Now keeps a pristine copy, which it can use to revert changes on command and
    between repeated calls.

--- tests/corrupt.py
+++ tests/corrupt.py
@@ -1,19 +1,45 @@
 #!/usr/bin/env python3.1
-import sys
-import optparse
+import optparse, os, shutil, sys
+
+def revert(path):
+    bkup = path + os.path.extsep + options.backup_suffix
+    if os.access(bkup, os.R_OK):
+        shutil.copy2(bkup, path)
+        os.remove(bkup)
+
+
+def corrupt(path, offset, value):
+    if options.backup:
+        shutil.copy2(path, path + os.path.extsep + options.backup_suffix)
+    with open(path,'r+b',buffering=0) as f:
+        f.seek(offset)
+        f.write(bytes([value]))
+
 
 parser = optparse.OptionParser(usage='usage: %prog file byte-offset replacment-value')
+parser.add_option("", "--revert", action="store_true", default=False,
+        help="restore the path to pristine condition if possible.")
+parser.add_option("-b", "--backup", action="store_true", default=True, dest="backup",
+        help="create a backup of the uncorrupted original [default: %default]")
+parser.add_option("", "--no-backup", action="store_false", dest="backup",
+        help="do not create a backup of the uncorrupted original.")
+parser.add_option("", "--backup-suffix", type="string", default="pristine",
+        help="suffix for uncorrupted copy of the file [default: %default]")
 (options, args) = parser.parse_args()
 
-if len(args) != 3:
+if options.revert:
+    if len(args) != 1:
+        parser.print_help()
+        sys.exit(1)
+elif len(args) != 3:
     parser.print_help()
     sys.exit(1)
 
 path   = args[0]
-offset = eval(args[1])
-value  = eval(args[2])
+revert(path)
 
-with open(path,'r+b',buffering=0) as f:
-    f.seek(offset)
-    f.write(bytes([value]))
+if not options.revert:
+    offset = eval(args[1])
+    value  = eval(args[2])
+    corrupt(path, offset, value)
 
