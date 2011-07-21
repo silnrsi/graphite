55f4fc58b26b2a34ec9d094c87820344c02f4bf8
Tim Eves <tim_eves@sil.org>
2011-07-21 15:16:25 +0700

    Add a sparse array class
    Implement a bitmap based read-only sparse array using no branches for look-up
    and minimal overhead.

---
+++ src/Sparse.cpp
@@ -0,0 +1,88 @@
+/*  GRAPHITE2 LICENSING
+
+    Copyright 2010, SIL International
+    All rights reserved.
+
+    This library is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 2.1 of License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should also have received a copy of the GNU Lesser General Public
+    License along with this library in the file named "LICENSE".
+    If not, write to the Free Software Foundation, Inc., 59 Temple Place,
+    Suite 330, Boston, MA 02111-1307, USA or visit their web page on the
+    internet at http://www.fsf.org/licenses/lgpl.html.
+
+Alternatively, the contents of this file may be used under the terms of the
+Mozilla Public License (http://mozilla.org/MPL) or the GNU General Public
+License, as published by the Free Software Foundation, either version 2
+of the License or (at your option) any later version.
+*/
+
+#include "Sparse.h"
+
+using namespace graphite2;
+
+namespace
+{
+	inline unsigned int bit_set_count(unsigned int v)
+	{
+		v = v - ((v >> 1) & 0x55555555);                    // reuse input as temporary
+		v = (v & 0x33333333) + ((v >> 2) & 0x33333333);     // temp
+		const unsigned int c = (((v + (v >> 4)) & 0xF0F0F0F) * 0x1010101) >> 24; // count
+		return c;
+	}
+
+	inline unsigned int zero_trailing_count(uint32 v)
+	{
+		static const unsigned int MultiplyDeBruijnBitPosition[32] =
+		{
+		  0, 1, 28, 2, 29, 14, 24, 3, 30, 22, 20, 15, 25, 17, 4, 8,
+		  31, 27, 13, 23, 21, 19, 16, 7, 26, 12, 18, 6, 11, 5, 10, 9
+		};
+
+		return MultiplyDeBruijnBitPosition[(uint32((v & -v) * 0x077CB531U)) >> 27];
+	}
+
+	inline unsigned int log2(uint32 v)
+	{
+		static const unsigned int MultiplyDeBruijnBitPosition[32] =
+		{
+		  0, 9, 1, 10, 13, 21, 2, 29, 11, 14, 16, 18, 22, 25, 3, 30,
+		  8, 12, 20, 28, 15, 17, 24, 7, 19, 27, 23, 6, 26, 5, 4, 31
+		};
+
+		v |= v >> 1; // first round down to one less than a power of 2
+		v |= v >> 2;
+		v |= v >> 4;
+		v |= v >> 8;
+		v |= v >> 16;
+
+		return MultiplyDeBruijnBitPosition[uint32((v * 0x07C4ACDDU) >> 27)];
+	}
+}
+
+
+sparse::~sparse() throw()
+{
+	free(m_map);
+	free(m_values);
+}
+
+
+sparse::value sparse::operator [] (int k) const
+{
+	const chunk & 		c = m_map[k/SIZEOF_CHUNK];
+	const unsigned int	o = k % SIZEOF_CHUNK;
+	const unsigned long b = 1 << (SIZEOF_CHUNK-1 - o);
+	const size_t		bs = bit_set_count(~(c.mask | ((b << 1) - 1)));
+
+	return bool((c.mask & b)*(k < m_limit))*m_values[c.offset + o - bs];
+}
+
---
+++ src/Sparse.h
@@ -0,0 +1,111 @@
+/*  GRAPHITE2 LICENSING
+
+    Copyright 2010, SIL International
+    All rights reserved.
+
+    This library is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 2.1 of License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should also have received a copy of the GNU Lesser General Public
+    License along with this library in the file named "LICENSE".
+    If not, write to the Free Software Foundation, Inc., 59 Temple Place, 
+    Suite 330, Boston, MA 02111-1307, USA or visit their web page on the 
+    internet at http://www.fsf.org/licenses/lgpl.html.
+
+Alternatively, the contents of this file may be used under the terms of the
+Mozilla Public License (http://mozilla.org/MPL) or the GNU General Public
+License, as published by the Free Software Foundation, either version 2
+of the License or (at your option) any later version.
+*/
+#pragma once
+
+#include "Main.h"
+
+namespace graphite2 {
+
+
+
+#define SIZEOF_CHUNK	32
+class sparse
+{
+	struct chunk
+	{
+		unsigned long	mask:SIZEOF_CHUNK;
+		uint16			offset;
+	};
+
+public:
+	typedef	uint16	key;
+	typedef uint16	value;
+
+	sparse() : m_limit(0), m_map(0), m_values(0) {}
+	~sparse() throw();
+
+	template<typename I>
+	sparse(I first, const I last);
+
+	value 	operator [] (int k) const;
+	operator bool () const { return m_map && m_values; }
+
+	size_t capacity() const { return m_limit; }
+	size_t size()     const { return m_limit; }
+
+private:
+	chunk & get_chunk(key & k);
+	const chunk & get_chunk(key & k) const { return const_cast<sparse *>(this)->get_chunk(k); }
+
+	key		m_limit;
+	chunk *	m_map;
+	value *	m_values;
+};
+
+
+template <typename I>
+sparse::sparse(I attr, const I last)
+: m_limit(0), m_map(0), m_values(0)
+{
+	// Find the maximum extent of the key space.
+	size_t n_values=0;
+	for (I i = attr; i != last; ++i, ++n_values)
+	{
+		const key k = i->id;
+		if (k >= m_limit) m_limit = k+1;
+	}
+
+	m_map    = grzeroalloc<chunk>((m_limit + SIZEOF_CHUNK-1)/SIZEOF_CHUNK);
+	m_values = gralloc<value>(n_values);
+
+	if (!*this)
+	{
+		free(m_map); free(m_values);
+		m_map = 0; m_values = 0;
+		return;
+	}
+
+	chunk * ci = m_map;
+	value * vi = m_values;
+	for (key base = 0; attr != last; ++attr, ++vi)
+	{
+		const typename I::value_type v = *attr;
+		const key chunks_diff = (v.id - base) / SIZEOF_CHUNK;
+
+		if (chunks_diff)
+		{
+			ci   += chunks_diff;
+			base += chunks_diff * SIZEOF_CHUNK;
+			ci->offset = vi - m_values;
+		}
+
+		ci->mask |= 1 << (SIZEOF_CHUNK - 1 - ((v.id - base) % SIZEOF_CHUNK));
+		*vi = v.value;
+	}
+}
+
+} // namespace graphite2
