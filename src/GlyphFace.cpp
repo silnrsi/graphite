1bf9333907a9567bb233849a56626478188d1475
Tim Eves <tim_eves@sil.org>
2011-07-21 15:19:45 +0700

    Integrate sparse glyph attribute array.
    Added Glat table attribute iterators.
    Rework readAttrs to use the glat_iterators.
    replace readAttr and flat C array with sparse array class to store per glyph
    attributes.

--- src/GlyphFace.cpp
+++ src/GlyphFace.cpp
@@ -24,6 +24,7 @@ Mozilla Public License (http://mozilla.org/MPL) or the GNU General Public
 License, as published by the Free Software Foundation, either version 2
 of the License or (at your option) any later version.
 */
+#include <iterator>
 #include "GlyphFace.h"
 #include "XmlTraceLog.h"
 #include "GlyphFaceCache.h"
@@ -31,6 +32,43 @@ of the License or (at your option) any later version.
 
 
 using namespace graphite2;
+
+namespace
+{
+	struct glyph_attr { uint16 id, value; };
+
+	class glat_iterator : public std::iterator<std::input_iterator_tag, glyph_attr>
+	{
+	public:
+		glat_iterator() : _p(0), _n(0) {}
+		glat_iterator(const void * glat) : _p(reinterpret_cast<const byte *>(glat)), _n(0) {
+			operator++();
+		}
+
+		glat_iterator & operator ++ () 		{ ++_v.id; if (_n==0) { _v.id = *_p++; _n = *_p++; } --_n; _v.value = read16(_p); return *this; }
+		glat_iterator   operator ++ (int) 	{ glat_iterator tmp(*this); operator++(); return tmp; }
+
+		bool operator == (const glat_iterator & rhs) { return _p == rhs._p; }
+		bool operator != (const glat_iterator & rhs) { return !operator==(rhs); }
+
+		value_type 			operator * () const { return _v; }
+		const value_type *	operator ->() const { return &_v; }
+
+	protected:
+		const byte * _p;
+		value_type  _v;
+		unsigned int _n;
+	};
+
+	class glat2_iterator : public glat_iterator
+	{
+	public:
+		glat2_iterator(const void * glat) { _p = reinterpret_cast<const byte *>(glat); _n = 0; operator++(); }
+
+		glat_iterator & operator ++ () 		{ ++_v.id; if (!_n) { _v.id = read16(_p); _n = read16(_p); } --_n; _v.value = read16(_p); return *this; }
+		glat_iterator   operator ++ (int) 	{ glat_iterator tmp(*this); operator++(); return tmp; }
+	};
+}
 
 GlyphFace::GlyphFace(const GlyphFaceCacheHeader& hdr, unsigned short glyphid)
 {
@@ -78,59 +116,99 @@ GlyphFace::GlyphFace(const GlyphFaceCacheHeader& hdr, unsigned short glyphid)
             glocs = swap16(((uint16 *)hdr.m_pGloc)[4+glyphid]);
             gloce = swap16(((uint16 *)hdr.m_pGloc)[5+glyphid]);
         }
-        if (glocs >= hdr.m_lGlat || gloce > hdr.m_lGlat)
-            m_attrs = NULL;
-        else
-            readAttrs(hdr.m_pGlat, glocs, gloce, hdr.m_numAttrs, hdr.m_fGlat);
+        if (glocs < hdr.m_lGlat && gloce <= hdr.m_lGlat)
+        {
+//            m_attrs = grzeroalloc<uint16>(hdr.m_numAttrs);
+//            readAttrs(hdr.m_pGlat + glocs, hdr.m_pGlat + gloce, m_attrs, hdr.m_numAttrs, hdr.m_fGlat);
+        	if (hdr.m_fGlat < 0x00020000)
+        		new (&m_attrs) sparse(glat_iterator(hdr.m_pGlat + glocs), glat_iterator(hdr.m_pGlat + gloce));
+        	else
+        		new (&m_attrs) sparse(glat2_iterator(hdr.m_pGlat + glocs), glat2_iterator(hdr.m_pGlat + gloce));
+
+        }
     }
-    else
-        m_attrs = NULL;
 #ifndef DISABLE_TRACING
     XmlTraceLog::get().closeElement(ElementGlyphFace);
 #endif
 }
 
 
-void GlyphFace::readAttrs(const void *pGlat, int start, int end, size_t num, uint32 format) 
+//void GlyphFace::readAttrs(const byte *glat, const byte * const glat_end, uint16 * attrs, size_t num, uint32 format)
+//{
+//	glat_iterator::value_type v;
+//
+//	if (format < 0x00020000)
+//	{
+//		for (glat_iterator i = glat, end = glat_end; i != end; ++i)
+//		{
+//			v = *i;
+//			if (v.id >= num) break;
+//			attrs[v.id] = v.value;
+//		}
+//	}
+//	else
+//	{
+//		for (glat2_iterator i = glat, end = glat_end; i != end; ++i)
+//		{
+//			v = *i;
+//			if (v.id >= num) break;
+//			attrs[v.id] = v.value;
+//		}
+//	}
+//
+//	if (v.id >= num) {
+//#ifndef DISABLE_TRACING
+//		XmlTraceLog::get().warning("Invalid glat entry: attr id %d max id %d", v.id, num);
+//#endif
+//	}
+//
+//	const byte * 		glat 		= static_cast<const byte *>(pGlat) + start,
+//			   * const 	glat_end 	= glat + end - start;
+
+
+//	while (glat < glat_end)
+//	{
+//		uint16 * attr, * attr_end;
+//
+//		if (format < 0x00020000)
+//		{
+//			attr = attrs + *glat++; attr_end = attr + *glat++;
+//		}
+//		else
+//		{
+//			attr = attrs + read16(glat); attr_end = attr + read16(glat);
+//		}
+//
+//		if (attr_end > attrs + num)
+//		{
+//#ifndef DISABLE_TRACING
+//			XmlTraceLog::get().warning("Invalid glat entry: attr id %d count %d", *attr, attr_end - attr);
+//#endif
+//			return;
+//		}
+//		while (attr != attr_end)
+//		{
+//			*attr++ = read16(glat);
+//			logAttr(attrs, attr-1);
+//		}
+//	}
+//}
+
+
+inline
+void GlyphFace::logAttr(const uint16 attrs[], const uint16 * attr)
 {
-    m_attrs = grzeroalloc<uint16>(num);
-    while (start < end)
-    {
-        unsigned int attr;
-        unsigned int count;
-        if (format < 0x00020000)
-        {
-            attr = ((uint8 *)pGlat)[start];
-            count = ((uint8 *)pGlat)[start + 1];
-        }
-        else
-        {
-            attr = swap16(*(uint16 *)((uint8 *)pGlat + start));
-            count = swap16(*(uint16 *)((uint8 *)pGlat + start + 2));
-        }
-        if (attr + count > num)
-        {
 #ifndef DISABLE_TRACING
-            XmlTraceLog::get().warning("Invalid glat entry: attr id %d count %d", attr, count);
+	if (XmlTraceLog::get().active())
+	{
+		XmlTraceLog::get().openElement(ElementAttr);
+		XmlTraceLog::get().addAttribute(AttrAttrId, attr - attrs);
+		XmlTraceLog::get().addAttribute(AttrAttrVal, *attr);
+		XmlTraceLog::get().closeElement(ElementAttr);
+	}
 #endif
-            return;
-        }
-        for (unsigned int i = 0; i < count; i++)
-        {
-            m_attrs[attr + i] = swap16(((uint16 *)((char *)pGlat + start))[1 + i]);
-#ifndef DISABLE_TRACING
-            if (XmlTraceLog::get().active())
-            {
-                XmlTraceLog::get().openElement(ElementAttr);
-                XmlTraceLog::get().addAttribute(AttrAttrId, attr + i);
-                XmlTraceLog::get().addAttribute(AttrAttrVal, m_attrs[attr+i]);
-                XmlTraceLog::get().closeElement(ElementAttr);
-            }
-#endif
-        }
-        start += 2 * (count + 1);
-    }
 }
+
 
 uint16 GlyphFace::getMetric(uint8 metric) const
 {
