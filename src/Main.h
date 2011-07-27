a1cd5946f14683b1508aa2121ece5d9364370f0b
Martin Hosken <martin_hosken@sil.org>
2011-07-27 16:11:29 +0700

    Add cmake code to set WORDS_BIGENDIAN

--- src/Main.h
+++ src/Main.h
@@ -28,6 +28,7 @@ of the License or (at your option) any later version.
 
 #include <cstdlib>
 #include "graphite2/Types.h"
+#include "config.h"
 
 #define MAKE_TAG(a,b,c,d) ((a << 24UL) + (b << 16UL) + (c << 8UL) + (d))
 
