d3bb71f7bfbd8391c32c860e79359d09d12a115d
Martin Hosken <martin_hosken@sil.org>
2011-07-27 16:38:27 +0700

    Tidy up copyright and licensing for contrib/perl

--- contrib/perl/lib/Text/Graphite2.pm
+++ contrib/perl/lib/Text/Graphite2.pm
@@ -97,3 +97,37 @@ http://graphite.sil.org/
 =cut
 
 1; # End of Text::Graphite2
+
+__END__
+
+=begin copyright
+
+GRAPHITE2 LICENSING
+
+    Copyright 2010, SIL International
+    All rights reserved.
+
+    This library is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 2.1 of License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should also have received a copy of the GNU Lesser General Public
+    License along with this library in the file named "LICENSE".
+    If not, write to the Free Software Foundation, Inc., 59 Temple Place, 
+    Suite 330, Boston, MA 02111-1307, USA or visit their web page on the 
+    internet at http://www.fsf.org/licenses/lgpl.html.
+
+Alternatively, the contents of this file may be used under the terms of the
+Mozilla Public License (http://mozilla.org/MPL) or the GNU General Public
+License, as published by the Free Software Foundation, either version 2
+of the License or (at your option) any later version.
+
+    Author: Simon Cozens <simon@cpan.org>
+
+=end copyright
