935dd7e7de8cdc2b50cce9632b97f569bd5853cb
Martin Hosken <martin_hosken@sil.org>
2011-08-02 13:58:17 +0700

    Aargh, this file got forgotten

---
+++ contrib/android/jni/load_gr.h
@@ -0,0 +1,42 @@
+/*  GRAPHITE2 LICENSING
+
+    Copyright 2010, SIL International
+    All rights reserved.
+
+    This library is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 2.1 of License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should also have received a copy of the GNU Lesser General Public
+    License along with this library in the file named "LICENSE".
+    If not, write to the Free Software Foundation, Inc., 59 Temple Place, 
+    Suite 330, Boston, MA 02111-1307, USA or visit their web page on the 
+    internet at http://www.fsf.org/licenses/lgpl.html.
+
+    Alternatively, you may use this library under the terms of the Mozilla
+    Public License (http://mozilla.org/MPL) or under the GNU General Public
+    License, as published by the Free Sofware Foundation; either version
+    2 of the license or (at your option) any later version.
+*/
+
+#include "SkTypeface.h"
+#include "graphite2/Font.h"
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+gr_face *gr_face_from_tf(SkTypeface *tf, int *rtl);
+SkTypeface *tf_from_name(const char *name);
+
+#ifdef __cplusplus
+}
+#endif
+
+
