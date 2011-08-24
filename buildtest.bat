28bcaf0deb5f481954135f94efa24d8439999063
Martin Hosken <martin_hosken@sil.org>
2011-08-24 11:15:19 +0700

    Start fixing buildtest.bat

--- buildtest.bat
+++ buildtest.bat
@@ -1,3 +1,6 @@
-cmake -G "NMake Makefiles" -D CMAKE_BUILD_TYPE:STRING=Release
+call vcvars32
+mkdir build
+cd build
+cmake -G "NMake Makefiles" -D CMAKE_BUILD_TYPE:STRING=Release -D ENABLE_COMPARE_RENDERER:BOOL=OFF ..
 nmake
 nmake test
