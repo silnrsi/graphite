f130b7957883a0bcf6cca9589531218f6110b822
Martin Hosken <martin_hosken@sil.org>
2011-08-24 11:23:52 +0700

    All changes have to go via the repo :\(

--- buildtest.bat
+++ buildtest.bat
@@ -1,6 +1,6 @@
 call vcvars32
 mkdir build
 cd build
-cmake -G "NMake Makefiles" -D CMAKE_BUILD_TYPE:STRING=Release -D ENABLE_COMPARE_RENDERER:BOOL=OFF ..
+cmake -G "NMake Makefiles" -D CMAKE_BUILD_TYPE:STRING=Release -D ENABLE_COMPARE_RENDERER:BOOL=OFF -D VM_MACHINE_TYPE:STRING=Call ..
 nmake
 nmake test
