3adaab84995f6854555c8ac56656aa89adc29fe6
Tim Eves <tim_eves@sil.org>
2011-09-19 15:52:41 +0700

    Fix for the buildtest.bat fix

--- buildtest.bat
+++ buildtest.bat
@@ -1,8 +1,8 @@
 call vcvars32
+rmdir /s /q build
 mkdir build
 cd build
 cmake -G "NMake Makefiles" -D CMAKE_BUILD_TYPE:STRING=Release -D ENABLE_COMPARE_RENDERER:BOOL=OFF -D VM_MACHINE_TYPE:STRING=Call ..
 nmake
-if errorlevel 0 nmake test
-cd ..
-rmdir /s /q build
+if errorlevel 0 (nmake test)
+
