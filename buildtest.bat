33c32c227befa24f0e873882723aa6ca73ba9b54
Tim Eves <tim_eves@sil.org>
2011-09-19 15:32:41 +0700

    Check in Windows teamcity fix

--- buildtest.bat
+++ buildtest.bat
@@ -3,4 +3,4 @@ mkdir build
 cd build
 cmake -G "NMake Makefiles" -D CMAKE_BUILD_TYPE:STRING=Release -D ENABLE_COMPARE_RENDERER:BOOL=OFF -D VM_MACHINE_TYPE:STRING=Call ..
 nmake
-nmake test
+rmdir /s /q build
