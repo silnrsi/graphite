e9daa0a0b3cd932b269ae1e13303b8c983dc04ae
Tim Eves <tim_eves@sil.org>
2011-09-19 15:41:00 +0700

    Fix for the buildtest.bat fix

--- buildtest.bat
+++ buildtest.bat
@@ -3,4 +3,6 @@ mkdir build
 cd build
 cmake -G "NMake Makefiles" -D CMAKE_BUILD_TYPE:STRING=Release -D ENABLE_COMPARE_RENDERER:BOOL=OFF -D VM_MACHINE_TYPE:STRING=Call ..
 nmake
+if errorlevel 0 nmake test
+cd ..
 rmdir /s /q build
