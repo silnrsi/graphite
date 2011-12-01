5a0963e92940e21d8d75defecca89ccf8fe4f6d5
Tim Eves <tim_eves@sil.org>
2011-12-01 17:37:05 +0700

    Fix gr_face_dumbRendering flag failing to load valid dumb fonts

--- src/Face.h
+++ src/Face.h
@@ -130,7 +130,7 @@ private:
     unsigned short numGlyphs() const { return m_pGlyphFaceCache->m_nGlyphs; }
 
 public:
-    bool readGlyphs(unsigned int faceOptions);
+    bool readGlyphs(uint32 faceOptions);
     bool readGraphite();
     bool readFeatures() { return m_Sill.readFace(*this); }
     const Silf *chooseSilf(uint32 script) const;
