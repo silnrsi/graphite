8d9b96600b425ab22c04044e5e866d6cb1c8b88c
Tim Eves <tim_eves@sil.org>
2012-10-02 09:42:38 +0700

    Fix several signatures to avoid uint16->size_t->uint16 conversions
    When those methods were inlined the optimiser probably collapsed those
    conversions, in the cases were it isn't this causes clang to complain

--- src/FileFace.cpp
+++ src/FileFace.cpp
@@ -25,19 +25,12 @@ License, as published by the Free Software Foundation, either version 2
 of the License or (at your option) any later version.
 */
 #include <cstring>
-//#include "graphite2/Segment.h"
-//#include "inc/CmapCache.h"
-//#include "inc/Endian.h"
 #include "inc/FileFace.h"
-//#include "inc/GlyphFace.h"
-//#include "inc/SegCacheStore.h"
-//#include "inc/Segment.h"
-//#include "inc/NameTable.h"
 
-
-using namespace graphite2;
 
 #ifndef GRAPHITE2_NFILEFACE
+
+using namespace graphite2;
 
 FileFace::FileFace(const char *filename)
 : _file(fopen(filename, "rb")),
