4eb47eadb72948155332bf70c0dc6b842b185420
Martin Hosken <martin_hosken@sil.org>
2013-10-29 16:44:07 +0700

    Faff with copyright dates to reflect the actual file creation year

--- src/inc/Endian.h
+++ src/inc/Endian.h
@@ -1,6 +1,7 @@
-/*-----------------------------------------------------------------------------
-Copyright (C) 2011 SIL International
-Responsibility: Tim Eves
+/*  GRAPHITE2 LICENSING
+
+    Copyright 2011, SIL International
+    All rights reserved.
 
     This library is free software; you can redistribute it and/or modify
     it under the terms of the GNU Lesser General Public License as published
@@ -22,17 +23,21 @@ Alternatively, the contents of this file may be used under the terms of the
 Mozilla Public License (http://mozilla.org/MPL) or the GNU General Public
 License, as published by the Free Software Foundation, either version 2
 of the License or (at your option) any later version.
+*/
 
+/*
 Description:
-A set of fast template based decoders for decoding values of any C integer
-type up to long int size laid out with most significant byte first or least
-significant byte first (aka big endian or little endian).  These are CPU
-byte order agnostic and will function the same regardless of the CPUs native
-byte order.
-Being template based means if the either le or be class is not used then
-template code of unused functions will not be instantiated by the compiler
-and thus shouldn't cause any overhead.
------------------------------------------------------------------------------*/
+    A set of fast template based decoders for decoding values of any C integer
+    type up to long int size laid out with most significant byte first or least
+    significant byte first (aka big endian or little endian).  These are CPU
+    byte order agnostic and will function the same regardless of the CPUs native
+    byte order.
+
+    Being template based means if the either le or be class is not used then
+    template code of unused functions will not be instantiated by the compiler
+    and thus shouldn't cause any overhead.
+*/
+
 #include <cstddef>
 
 #pragma once
