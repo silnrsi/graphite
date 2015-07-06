08735d0d0fcbbfb3a181d905bb013add07a2bb46
Martin Hosken <martin_hosken@sil.org>
2015-07-06 13:00:05 +0700

    Add timeouts to fuzztest awami

--- tests/full-fuzz-test.sh
+++ tests/full-fuzz-test.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 TESTSDIR=$(dirname $0)
-${TESTSDIR}/fuzzcomparerender Awami_test awami_tests -r --nolimit --passes=1 "$@"
+${TESTSDIR}/fuzzcomparerender Awami_test awami_tests -r --nolimit -t 20 --passes=1 "$@"
 ${TESTSDIR}/fuzzcomparerender Padauk my_HeadwordSyllables --passes=1 "$@"
-${TESTSDIR}/fuzzcomparerender Annapurnarc2 udhr_nep  --passes=1 "$@" 
-${TESTSDIR}/fuzzcomparerender charis_r_gr udhr_yor  --passes=1 "$@"
+${TESTSDIR}/fuzzcomparerender Annapurnarc2 udhr_nep --passes=1 "$@" 
+${TESTSDIR}/fuzzcomparerender charis_r_gr udhr_yor --passes=1 "$@"
 ${TESTSDIR}/fuzzcomparerender Scheherazadegr udhr_arb -r --passes=1 "@"
 
 
