deff01988b6463480f84f286655d8b81ec14624d
Tim Eves <tim_eves@sil.org>
2018-04-26 02:58:07 +0700

    Make gr_make_seg more a bit robust
    Allow it to accept a null face pointer and return nullptr rather than
    requiring the caller to have checked it.

--- src/gr_segment.cpp
+++ src/gr_segment.cpp
@@ -99,6 +99,8 @@ size_t gr_count_unicode_characters(gr_encform enc, const void* buffer_begin, con
 
 gr_segment* gr_make_seg(const gr_font *font, const gr_face *face, gr_uint32 script, const gr_feature_val* pFeats, gr_encform enc, const void* pStart, size_t nChars, int dir)
 {
+    if (!face) return nullptr;
+
     const gr_feature_val * tmp_feats = 0;
     if (pFeats == 0)
         pFeats = tmp_feats = static_cast<const gr_feature_val*>(face->theSill().cloneFeatures(0));
