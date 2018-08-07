49c0a07bd63c03a99be0a8c5f40911b907bd657d
Tim Eves <tim_eves@sil.org>
2018-08-07 14:04:10 +0700

    Formating tidy ups

--- tests/corrupt.py
+++ tests/corrupt.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
-import optparse, os, shutil, sys
+import optparse
+import os
+import shutil
+import sys
+
 
 def revert(path):
     bkup = path + os.path.extsep + options.backup_suffix
@@ -11,20 +15,27 @@ def revert(path):
 def corrupt(path, offset, value):
     if options.backup:
         shutil.copy2(path, path + os.path.extsep + options.backup_suffix)
-    with open(path,'r+b',buffering=0) as f:
+    with open(path, 'r+b', buffering=0) as f:
         f.seek(offset)
         f.write(bytes(chr(value)))
 
 
-parser = optparse.OptionParser(usage='usage: %prog file byte-offset replacment-value')
-parser.add_option("", "--revert", action="store_true", default=False,
-        help="restore the path to pristine condition if possible.")
-parser.add_option("-b", "--backup", action="store_true", default=True, dest="backup",
-        help="create a backup of the uncorrupted original [default: %default]")
-parser.add_option("", "--no-backup", action="store_false", dest="backup",
-        help="do not create a backup of the uncorrupted original.")
-parser.add_option("", "--backup-suffix", type="string", default="pristine",
-        help="suffix for uncorrupted copy of the file [default: %default]")
+parser = optparse.OptionParser(
+    usage='usage: %prog file byte-offset replacment-value')
+parser.add_option("", "--revert",
+                  action="store_true", default=False,
+                  help="restore the path to pristine condition if possible.")
+parser.add_option("-b", "--backup",
+                  action="store_true", default=True, dest="backup",
+                  help="create a backup of the uncorrupted original"
+                       " [default: %default]")
+parser.add_option("", "--no-backup",
+                  action="store_false", dest="backup",
+                  help="do not create a backup of the uncorrupted original.")
+parser.add_option("", "--backup-suffix",
+                  type="string", default="pristine",
+                  help="suffix for uncorrupted copy of the file"
+                       " [default: %default]")
 (options, args) = parser.parse_args()
 
 if options.revert:
@@ -35,11 +46,10 @@ elif len(args) != 3:
     parser.print_help()
     sys.exit(1)
 
-path   = args[0]
+path = args[0]
 revert(path)
 
 if not options.revert:
     offset = int(eval(args[1]))
-    value  = int(eval(args[2]))
+    value = int(eval(args[2]))
     corrupt(path, offset, value)
-
